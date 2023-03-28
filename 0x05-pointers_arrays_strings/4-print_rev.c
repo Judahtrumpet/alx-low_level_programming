6a7c7b5a92811613d8afbc90dff9c86fffa91879
judahtrumpet <slimjudahdaniels@gmail.com>
2023-03-27 20:00:46 -0700

    pointers,array

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: string variable
+ * Return: is 0
+ */
+void print_rev(char *s)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		j++;
+	for (i = j ; i >= 0 ; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
