4712dba458f6a417b540dd02d705f11b96b86f13
judahtrumpet <slimjudahdaniels@gmail.com>
2023-03-27 20:34:55 -0700

    p,a

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,8 +11,10 @@ void print_rev(char *s)
 	int j = 0;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
+	{
 		j++;
-	for (i = j ; i >= 0 ; i--)
-		_putchar(s[i]);
+		for (i = j ; i >= 0 ; i--)
+			_putchar(s[i]);
+	}
 	_putchar('\n');
 }
