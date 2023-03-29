11744c9520bd868c01e826f60747937d9ccbdfb6
judahtrumpet <slimjudahdaniels@gmail.com>
2023-03-29 16:56:41 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints numerical signs
+ * @n: the varaible
+ * Return: return 1 if positive, 0 if it equals to and -1 if it is negative
+ */
+int print_sign(int n)
+{
+	        if (n > 0)
+        {
+                _putchar('+');
+                return (1);
+        }
+        else if (n == 0)
+        {
+                _putchar('0');
+                return (0);
+        }
+        else if (n < 0)
+        {
+                _putchar('-');
+                return (-1);
+	}
+	return (0);
+}	
